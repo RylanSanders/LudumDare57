de3eaade9f5c7a559d62d96343b353a97095ad1b
rsanders <sandersrylan2@gmail.com>
2025-04-06 11:38:56 -0500

    Add High Score

--- game_controller.gd
+++ game_controller.gd
@@ -3,25 +3,61 @@ extends Node2D
 @export var death_menu:PackedScene
 @onready var spear:Node2D = get_node("Spear")
 @onready var depth_label:Label = get_node("CanvasLayer/DepthLabel")
+@onready var high_score_label: Label = get_node("CanvasLayer/HighScoreLabel")
 
 var DEPTH_LABEL := "DEPTH: "
+var HIGH_SCORE_LABEL := "HIGH SCORE: "
+var HIGH_SCORE_VAR := "high_score"
+var SCORES_SECTION_VAR := "scores"
+var cnf_path:= "user://scores.ini"
+var current_high_score:=0.0
+var last_high_score:=0.0
+
 func game_over():
 	get_tree().paused=true
 	var new_death_menu :PopupMenu = death_menu.instantiate()
+	record_high_score()
 	new_death_menu.GameController=self
 	add_child(new_death_menu)
 	
 	new_death_menu.popup_centered()
 
-func retry():
+func record_high_score() -> void:
+	var new_score = calculate_depth()
+	if current_high_score > last_high_score:
+		var conf := ConfigFile.new()
+		conf.set_value(SCORES_SECTION_VAR,HIGH_SCORE_VAR, current_high_score)
+		var error := conf.save(cnf_path)
+	
+
+func _ready() -> void:
+	get_saved_high_score()
+
+func get_saved_high_score():
+	var conf := ConfigFile.new()
+	
+	var error := conf.load(cnf_path)
+	if error==OK:
+		last_high_score = conf.get_value(SCORES_SECTION_VAR,HIGH_SCORE_VAR,0  )
+		current_high_score = last_high_score
+		high_score_label.text = HIGH_SCORE_LABEL + str(current_high_score)
+
+func retry()  -> void:
 	get_tree().paused=false
 	get_tree().reload_current_scene()
 	
 	
 
-func main_menu():
+func main_menu()  -> void:
 	get_tree().paused=false
 	get_tree().change_scene_to_file("res://UI/MainMenu.tscn")
 
 func _process(delta: float) -> void:
-	depth_label.text = DEPTH_LABEL + str(snappedf(spear.global_position.y, 0.01))
+	var depth = calculate_depth()
+	depth_label.text = DEPTH_LABEL + str(depth)
+	if depth>current_high_score:
+		current_high_score = depth
+	
+
+func calculate_depth() -> float:
+	return snappedf(spear.global_position.y, 0.01)
