dca95bbed0d075325cc0b6d86127289308dc8910
rsanders <sandersrylan2@gmail.com>
2025-04-06 17:17:12 -0500

    Add Instructions

--- UI/main_menu.gd
+++ UI/main_menu.gd
@@ -3,3 +3,7 @@ extends Control
 
 func _on_options_pressed() -> void:
 	get_tree().change_scene_to_file("res://UI/OptionsMenu.tscn")
+
+
+func _on_instructions_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://UI/Instructions.tscn")
