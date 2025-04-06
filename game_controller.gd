677ef3a034ab649792897bec593ad1e522643587
rsanders <sandersrylan2@gmail.com>
2025-04-06 02:00:36 -0500

    Add Depth Label

--- game_controller.gd
+++ game_controller.gd
@@ -1,7 +1,10 @@
 extends Node2D
 
 @export var death_menu:PackedScene
-@onready var spear_forward:Node2D = get_node("Spear/Forward")
+@onready var spear:Node2D = get_node("Spear")
+@onready var depth_label:Label = get_node("CanvasLayer/DepthLabel")
+
+var DEPTH_LABEL := "DEPTH: "
 func game_over():
 	get_tree().paused=true
 	var new_death_menu :PopupMenu = death_menu.instantiate()
@@ -19,3 +22,6 @@ func retry():
 func main_menu():
 	get_tree().paused=false
 	get_tree().change_scene_to_file("res://UI/MainMenu.tscn")
+
+func _process(delta: float) -> void:
+	depth_label.text = DEPTH_LABEL + str(snappedf(spear.global_position.y, 0.01))
