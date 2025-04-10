9ea210219b50597d80a79cb802ad55a01c297c8a
rsanders <sandersrylan2@gmail.com>
2025-04-09 22:35:23 -0500

    Implement Shatter from that one reddit post

--- Obstacles/obstacle_base.gd
+++ Obstacles/obstacle_base.gd
@@ -4,6 +4,16 @@ class_name obstacle_base
 @export var durability:float = 10
 @export var gold_value:int = 0
 
+var is_dead :=false
+var death_timer:=0.0
+@export var death_delete_delay:float = 1.0
+
+func _process(delta: float) -> void:
+	if is_dead:
+		death_timer += delta
+		if death_timer>death_delete_delay:
+			queue_free()
+
 func _on_obstacle_entered(area: Area2D) -> void:
 	if area.name == "SpearTipArea":
 		var params:obstacle_params  = obstacle_params.new()
@@ -15,4 +25,12 @@ func _on_obstacle_entered(area: Area2D) -> void:
 
 #In the future this could be used to add animations and special effects on destroying an object
 func _on_destroyed():
-	queue_free()
+	monitorable = false
+	monitoring = false
+	is_dead = true
+	if shardEmitter != null:
+		shardEmitter.shatter()
+
+@onready var shardEmitter:ShardEmitter=$Sprite2D/ShardEmitter
+
+	
