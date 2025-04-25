b271b976ff1f0fe6390b1acdbd87b7e73f9602a2
rsanders <sandersrylan2@gmail.com>
2025-04-24 22:13:17 -0500

    Improve the plank

--- Obstacles/Scripts/plank.gd
+++ Obstacles/Scripts/plank.gd
@@ -1,1 +1,11 @@
 extends obstacle_base
+
+
+var timer:= 0.0
+var amplitude := 0.2
+
+func _ready() -> void:
+	timer = randf()
+func _process(delta: float) -> void:
+	timer+=delta
+	position.y += sin(timer) * amplitude
