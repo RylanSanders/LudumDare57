d3910d95d93d16ddb3293c1483a899b500fe978e
rsanders <sandersrylan2@gmail.com>
2025-04-27 12:48:04 -0500

    Save sound settings

--- UI/Scripts/options_menu.gd
+++ UI/Scripts/options_menu.gd
@@ -1,13 +1,20 @@
 extends Control
 
+@onready var SoundSlider = $GridContainer/SoundSlider
+@onready var MusicSlider = $GridContainer/MusicSlider
 
 func _on_sound_slider_value_changed(value: float) -> void:
-	AudioManger.change_sound_volumne(value/100)
+	AudioManger.change_sound_volumne(value)
 
 
 func _on_music_slider_value_changed(value: float) -> void:
-	AudioManger.change_music_volume(value/100)
+	AudioManger.change_music_volume(value)
 
 
 func _on_main_menu_button_pressed() -> void:
 	get_tree().change_scene_to_file("uid://bjqi2kveqdgok")
+	
+
+func _ready() -> void:
+	SoundSlider.value = AudioManger.sound_volumn
+	MusicSlider.value = AudioManger.music_node.volume_linear
--- sounds/audio_manger.gd
+++ sounds/audio_manger.gd
@@ -4,6 +4,12 @@ class_name audio_manager
 
 var music_node: AudioStreamPlayer
 var sound_volumn:float = 1
+
+var cnf_path:= "user://settings.ini"
+var MUSIC_VAR := "MUSIC_VOLUME"
+var SOUND_VAR := "SOUND_VOLUME"
+var SOUND_SECTION := "SOUND_SETTINGS"
+
 func _ready() -> void:
 	music_node = AudioStreamPlayer.new()
 	process_mode = Node.PROCESS_MODE_ALWAYS
@@ -14,9 +20,26 @@ func _ready() -> void:
 	stream.loop_mode = AudioStreamWAV.LOOP_FORWARD
 	music_node.stream = stream
 	music_node.play()
+	load_settings()
 
 func change_music_volume(volume: float):
 	music_node.volume_linear = volume
+	record_settings()
 
 func change_sound_volumne(volume: float):
 	sound_volumn = volume
+	record_settings()
+
+func record_settings():
+	var conf := ConfigFile.new()
+	conf.set_value(SOUND_SECTION, SOUND_VAR, sound_volumn)
+	conf.set_value(SOUND_SECTION, MUSIC_VAR, music_node.volume_linear)
+	var error := conf.save(cnf_path)
+
+func load_settings():
+	var conf := ConfigFile.new()
+	
+	var error := conf.load(cnf_path)
+	if error==OK:
+		sound_volumn = conf.get_value(SOUND_SECTION, SOUND_VAR,1)
+		music_node.volume_linear =  conf.get_value(SOUND_SECTION, MUSIC_VAR, 1)
